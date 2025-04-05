5a78dcdb76623e53dcce596ecdb91eaca2c1d627
Calvin Nordstrom <calnordstrom@icloud.com>
2025-04-05 14:16:36 -0400

    docs: documented Divider

--- src/main/java/com/calvinnordstrom/cnboard/view/node/Divider.java
+++ src/main/java/com/calvinnordstrom/cnboard/view/node/Divider.java
@@ -2,24 +2,49 @@ package com.calvinnordstrom.cnboard.view.node;
 
 import javafx.scene.layout.Pane;
 
+/**
+ * A simple custom UI divider that supports two orientations: vertical and
+ * horizontal. This class extends {@link Pane} so it may be directly added to
+ * the node graph.
+ */
 public class Divider extends Pane {
-    public Divider(Type type) {
+    private Divider(Orientation type) {
         switch (type) {
             case VERTICAL -> getStyleClass().add("vertical-divider");
             case HORIZONTAL -> getStyleClass().add("horizontal-divider");
         }
     }
 
+    /**
+     * Creates a new vertical divider.
+     *
+     * @return a {@code Divider} of type {@link Orientation#VERTICAL}
+     */
     public static Divider vertical() {
-        return new Divider(Type.VERTICAL);
+        return new Divider(Orientation.VERTICAL);
     }
 
+    /**
+     * Creates a new horizontal divider.
+     *
+     * @return a {@code Divider} of type {@link Orientation#HORIZONTAL}
+     */
     public static Divider horizontal() {
-        return new Divider(Type.HORIZONTAL);
+        return new Divider(Orientation.HORIZONTAL);
     }
 
-    public enum Type {
+    /**
+     * Enum representing the orientation of the divider.
+     */
+    public enum Orientation {
+        /**
+         * Represents a vertical divider.
+         */
         VERTICAL,
-        HORIZONTAL
+
+        /**
+         * Represents a horizontal divider.
+         */
+        HORIZONTAL,
     }
 }
